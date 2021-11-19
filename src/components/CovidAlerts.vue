daf175efdef8adea89d4ac005419dd874472d5a4
yodanielgarcia <danielfelipegarciaforero@gmail.com>
2021-11-19 16:03:53 -0500

    covid alerts

--- src/components/CovidAlerts.vue
+++ src/components/CovidAlerts.vue
@@ -1,19 +1,28 @@
 <template>
   <b-container fluid="sm">
     <h1> Registre sus sintomas de alerta de covid </h1>
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show" style="margin-top:5%">
+    <b-form v-if="show" style="margin-top:5%">
         <b-row cols="2">
+            <label for="range-1">¿Positivo covid?</label>
+              <b-form-checkbox
+                  id="checkbox-1"
+                  v-model="form.PositiveCovid"
+                  name="checkbox-1"
+                  value="1"
+                  unchecked-value="0"
+                  >
+              </b-form-checkbox>
             <b-form-group id="input-group-3" label="Sintoma:" label-for="input-3">
                 <b-form-select
                     id="input-3"
-                    v-model="form.food"
-                    :options="foods"
+                    v-model="form.SymptomCovid"
+                    :options="sintomas"
                     required
                 ></b-form-select>
             </b-form-group>
             <b-col style="margin-top: 2px">
                 <label for="example-datepicker">Fecha de inicio</label>
-                <b-form-datepicker id="example-datepicker" v-model="form.dateStart" class="mb-2"></b-form-datepicker>
+                <b-form-datepicker id="example-datepicker" v-model="form.DateStart" class="mb-2"></b-form-datepicker>
             </b-col>
             <b-col style="margin-top: 2px">
                 <label for="range-1">Descripcion</label>
@@ -26,7 +35,7 @@
                 ></b-form-textarea>
             </b-col>
             <b-col style="margin-top: 5%;">
-                <b-button type="submit" variant="primary">Reportar</b-button>
+                <b-button type="button" @click="onSubmit" variant="primary">Reportar</b-button>
             </b-col>
         </b-row>
     </b-form>
@@ -34,38 +43,30 @@
 </template>
 
 <script>
+const axios = require('axios');
+var user = localStorage.getItem('UserLog');
+user = JSON.parse(user)
   export default {
     data() {
       return {
         form: {
-          email: '',
-          name: '',
-          maxcapacity:0,
-          food: null,
-          checked: []
+          Id_user:user.Id,
+          PositiveCovid: '',
+          SymptomCovid: '',
+          description: '',
+          DateStart:''
         },
-        foods: [{ text: 'Seleccion sintoma', value: null }, 'Positivo', 'Posible', 'Contacto con persona positiva'],
+        sintomas: [{ text: 'Seleccion sintoma', value: null }, 'Perdida del gusto', 'Gripa', 'Perdida del olfato'],
         show: true
       }
     },
     methods: {
-      onSubmit(event) {
-        event.preventDefault()
-        alert(JSON.stringify(this.form))
+      onSubmit() {
+      let body = this.form
+      console.log(body);
+      axios.post('https://dothggtj5k.execute-api.us-east-2.amazonaws.com/default/UdistritalLambdaAlertCovidUser', JSON.stringify(body) )
+      .then(response => response.data == 'ok' ? this.$router.push("/Reports") : alert('Contraseña o correo errado'));
       },
-      onReset(event) {
-        event.preventDefault()
-        // Reset our form values
-        this.form.email = ''
-        this.form.name = ''
-        this.form.food = null
-        this.form.checked = []
-        // Trick to reset/clear native browser form validation state
-        this.show = false
-        this.$nextTick(() => {
-          this.show = true
-        })
-      }
     }
   }
 </script>
